d54d33f85f398af939630c61395cbc40993c5ee9
rishonkumar <rishon.kumar.rahi@gmail.com>
2026-04-08 21:42:44 +0530

    setting up the project

---
+++ src/main/java/com/listmonk/app/MailListManagerApplication.java
@@ -0,0 +1,13 @@
+package com.listmonk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailListManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailListManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listmonk/app/MailListManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listmonk.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailListManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
